bf68b470a4b3cea5e986ff1da49b20ffa9eb94b3
Deepak Kamat <61752151+Deepak-1373@users.noreply.github.com>
2022-03-21 21:22:20 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,13 +4,10 @@ export const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbarLeft">
-        <img
-          alt="instagram-2.0"
-          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
-        ></img>
+        <h3>Upload</h3>
       </div>
       <div className="navbarRight">
-        <h3>Login</h3>
+        <h3>Images</h3>
       </div>
     </div>
   );
